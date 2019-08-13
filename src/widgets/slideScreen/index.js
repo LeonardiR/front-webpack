6268b8dd4b737da6d2b7575b2d8ae412d5960fa3
reguloleonardi <regulo.leonardi@gmail.com>
2019-08-13 21:16:03 +0200

    Slide Screen component

--- src/widgets/slideScreen/index.js
+++ src/widgets/slideScreen/index.js
@@ -5,10 +5,20 @@
         goToScreen: function () {
             for(var i=0; this.cta.length > i; i++){
                 this.cta[i].addEventListener('click', function (e) {
+                    slideScreen.hideScreens();
                     var id = e.target.getAttribute('data-screen'),
                         screen = document.getElementById(id);
-                    screen.classList.toggle('visible');
+                    if(id !== 'screen-main'){
+                        screen.classList.add('visible');
+                    }
                 });
+            }
+        },
+        hideScreens: function(){
+            for(var i=0; this.cta.length > i; i++){
+                var id = this.cta[i].getAttribute('data-screen'),
+                    screen = document.getElementById(id);
+                screen.classList.remove('visible');
             }
         },
         init:function () {
