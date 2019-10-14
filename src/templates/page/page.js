de12ef3a31bb0bcfd83c9d611b12d52f44366149
regulo.leonardi <regulo.leonardipirela@altran.com>
2019-10-14 15:22:33 +0200

    Resize iframe (test)

--- src/templates/page/page.js
+++ src/templates/page/page.js
@@ -1,11 +1,16 @@
 import './main.scss';
 
 class ResizeIframe  {
+    constructor(){
+        this.height = 0;
+        this.iframe = null;
+    }
     setIframeHeight() {
         setInterval(function() {
             if(window.frameElement){
-                let iframe = window.frameElement;
-                iframe.style.height = document.body.scrollHeight + 'px';
+                this.iframe = window.frameElement;
+                this.height = document.body.scrollHeight;
+                this.iframe.style.height = this.height + 'px';
             }else{
                 window.top.postMessage(document.body.scrollHeight, "*");
             }
